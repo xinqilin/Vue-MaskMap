0a5a7764a758a20b33340259d98d16536d0f8a19
BillLin1209 <zzx123bill@gmail.com>
2020-05-17 15:51:39 +0800

    modify html format document

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <button class="btn btn-primary">button</button>
+    <div class="row no-gutters">
+      <div class="col-sm-3">
+        <div class="toolbox">
+          <div class="sticky-top bg-white shadow-sm p-2">
+            <div class="form-group d-flex">
+              <label for="cityName" class="mr-2 col-form-label text-right">縣市</label>
+              <div class="flex-fill">
+                <select id="cityName" class="form-control"> </select>
+              </div>
+            </div>
+            <div class="form-group d-flex">
+              <label for="area" class="mr-2 col-form-label text-right">地區</label>
+              <div class="flex-fill">
+                <select id="area" class="form-control">
+                  <option value="">-- Select One --</option>
+                </select>
+              </div>
+            </div>
+            <p class="mb-0 small text-muted text-right">請先選擇區域查看（綠色表示還有口罩）</p>
+          </div>
+          <ul class="list-group">
+            <template>
+              <a class="list-group-item text-left">
+                <h3>藥局名稱</h3>
+                <p class="mb-0">
+                  成人口罩：1 | 兒童口罩：2
+                </p>
+                <p class="mb-0">
+                  地址：
+                  <a
+                    href="https://www.google.com.tw/maps/place/..."
+                    target="_blank"
+                    title="Google Map"
+                  >
+                    地址
+                  </a>
+                </p>
+              </a>
+            </template>
+          </ul>
+        </div>
+      </div>
+      <div class="col-sm-9">
+        <div id="map"></div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-
-export default {
-  name: 'App',
-  components: {
-  },
-};
-</script>
-
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "bootstrap/scss/bootstrap.scss";
+#map {
+  height: 100vh;
 }
-</style>
-<style lang="scss">
-@import 'bootstrap/scss/bootstrap.scss';
+.highlight {
+  background: #e9ffe3;
+}
+.toolbox {
+  height: 100vh;
+  overflow-y: auto;
+  a {
+    cursor: pointer;
+  }
+}
 </style>
