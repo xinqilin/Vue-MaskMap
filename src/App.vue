4e215e8115c1293cae7950039dae4f9a297913c4
BillLin1209 <zzx123bill@gmail.com>
2020-05-17 23:22:21 +0800

    大改版

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,185 @@
 <template>
-  <div id="app">
-    <div class="row no-gutters">
-      <div class="col-sm-3">
-        <div class="toolbox">
-          <div class="sticky-top bg-white shadow-sm p-2">
-            <div class="form-group d-flex">
-              <label for="cityName" class="mr-2 col-form-label text-right">縣市</label>
-              <div class="flex-fill">
-                <select id="cityName" class="form-control" v-model="checkCity"
-                @change="removeMapMarker(); updateMap();">
-                  <option value="">-- Select One --</option>
-                  <option v-for="c in Taiwan" :key="c.CityName" :value="c.CityName">
-                    {{c.CityName}}</option>
-                </select>
-              </div>
+  <div class="home row no-gutters">
+    <div class="col-sm-3">
+      <div v-if="cityName.length" class="toolbox">
+        <div class="sticky-top bg-white shadow-sm p-2">
+          <div class="form-group d-flex">
+            <label for="cityName" class="mr-2 col-form-label text-right">縣市</label>
+            <div class="flex-fill">
+              <select id="cityName" class="form-control"
+              v-model="select.city" @change="select.area = ''">
+                <option value="">-- Select One --</option>
+                <option :value="c.CityName" v-for="c in cityName" :key="c.CityName">
+                  {{ c.CityName }}
+                </option>
+              </select>
             </div>
-            <div class="form-group d-flex">
-              <label for="area" class="mr-2 col-form-label text-right">地區</label>
-              <div class="flex-fill">
-                <select id="area" class="form-control">
-                  <option value="">-- Select One --</option>
-                </select>
-              </div>
-            </div>
-            <p class="mb-0 small text-muted text-right">請先選擇區域查看（綠色表示還有口罩）</p>
           </div>
-          <ul class="list-group">
-            <template>
-              <a class="list-group-item text-left">
-                <h3>藥局名稱</h3>
-                <p class="mb-0">
-                  成人口罩：1 | 兒童口罩：2
-                </p>
-                <p class="mb-0">
-                  地址：
-                  <a
-                    href="https://www.google.com.tw/maps/place/..."
-                    target="_blank"
-                    title="Google Map"
-                  >
-                    地址
-                  </a>
-                </p>
-              </a>
-            </template>
-          </ul>
+          <div class="form-group d-flex">
+            <label for="area" class="mr-2 col-form-label text-right">地區</label>
+            <div class="flex-fill">
+              <select id="area" class="form-control" v-if="select.city.length"
+                v-model="select.area" @change="updateSelect">
+                <option value="">-- Select One --</option>
+                <option :value="a.AreaName"
+                  v-for="a in cityName.find((city) => city.CityName === select.city).AreaList"
+                  :key="a.AreaName">
+                  {{ a.AreaName }}
+                </option>
+              </select>
+            </div>
+          </div>
+          <p class="mb-0 small text-muted text-right">請先選擇區域查看（綠色表示還有口罩）</p>
         </div>
+
+        <ul class="list-group">
+          <template v-for="(item, key) in data">
+            <a class="list-group-item text-left" :key="key"
+              v-if="item.properties.county === select.city
+                && item.properties.town === select.area"
+              :class="{ 'highlight': item.properties.mask_adult || item.properties.mask_child}"
+              @click="penTo(item)">
+              <h3>{{ item.properties.name }}</h3>
+              <p class="mb-0">
+                成人口罩：{{ item.properties.mask_adult}} | 兒童口罩：{{ item.properties.mask_child}}
+              </p>
+              <p class="mb-0">地址：<a :href="`https://www.google.com.tw/maps/place/${item.properties.address}`"
+                target="_blank" title="Google Map">
+                {{ item.properties.address }}</a>
+              </p>
+            </a>
+          </template>
+        </ul>
       </div>
-      <div class="col-sm-9">
-        <div id="map"></div>
-      </div>
+    </div>
+    <div class="col-sm-9">
+      <div id="map"></div>
     </div>
   </div>
 </template>
 
+<script>
+// @ is an alias to /src
+import L from 'leaflet';
+import cityName from './assets/Taiwan.json';
+
+let osmMap = {};
+const iconsConfig = {
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+};
+const icons = {
+  green: new L.Icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    ...iconsConfig,
+  }),
+  grey: new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-grey.png',
+    ...iconsConfig,
+  }),
+};
+const osm = {
+  addMapMarker(x, y, item) {
+    const icon = item.mask_adult || item.mask_child ? icons.green : icons.grey;
+    L.marker([y, x], {
+      icon,
+    }).addTo(osmMap).bindPopup(`<strong>${item.name}</strong> <br>
+    口罩剩餘：<strong>成人 - ${item.mask_adult ? `${item.mask_adult} 個` : '未取得資料'}/ 兒童 - ${item.mask_child ? `${item.mask_child} 個` : '未取得資料'}</strong><br>
+    地址: <a href="https://www.google.com.tw/maps/place/${item.address}" target="_blank">${item.address}</a><br>
+    電話: ${item.phone}<br>
+    <small>最後更新時間: ${item.updated}</small>`);
+  },
+  removeMapMarker() {
+    osmMap.eachLayer((layer) => {
+      if (layer instanceof L.Marker) {
+        osmMap.removeLayer(layer);
+      }
+    });
+  },
+  penTo(x, y, item) {
+    const icon = item.mask_adult || item.mask_child ? icons.green : icons.grey;
+    osmMap.panTo(new L.LatLng(y, x));
+    L.marker([y, x], {
+      icon,
+    }).addTo(osmMap).bindPopup(`<strong>${item.name}</strong> <br>
+    口罩剩餘：<strong>成人 - ${item.mask_adult ? `${item.mask_adult} 個` : '未取得資料'}/ 兒童 - ${item.mask_child ? `${item.mask_child} 個` : '未取得資料'}</strong><br>
+    地址: <a href="https://www.google.com.tw/maps/place/${item.address}" target="_blank">${item.address}</a><br>
+    電話: ${item.phone}<br>
+    <small>最後更新時間: ${item.updated}</small>`).openPopup();
+  },
+};
+export default {
+  name: 'home',
+  data: () => ({
+    cityName,
+    data: {},
+    osmMap: {},
+    select: {
+      city: '臺北市',
+      area: '大安區',
+    },
+  }),
+  methods: {
+    updateMarker() {
+      const pharmacies = this.data.filter((pharmacy) => {
+        if (!this.select.area) {
+          return pharmacy.properties.county === this.select.city;
+        }
+        return pharmacy.properties.town === this.select.area;
+      });
+      pharmacies.forEach((pharmacy) => {
+        const { properties, geometry } = pharmacy;
+        osm.addMapMarker(
+          geometry.coordinates[0],
+          geometry.coordinates[1],
+          properties,
+        );
+      });
+      this.penTo(pharmacies[0]);
+    },
+    removeMarker() {
+      osm.removeMapMarker();
+    },
+    penTo(pharmacy) {
+      const { properties, geometry } = pharmacy;
+      osm.penTo(geometry.coordinates[0], geometry.coordinates[1], properties);
+    },
+    updateSelect() {
+      this.removeMarker();
+      this.updateMarker();
+      const pharmacy = this.data.find((item) => item.properties.town === this.select.area);
+      const { geometry, properties } = pharmacy;
+      osm.penTo(geometry.coordinates[0], geometry.coordinates[1], properties);
+    },
+  },
+  mounted() {
+    // OSM
+    osmMap = L.map('map', {
+      center: [25.03, 121.55],
+      zoom: 15,
+    });
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '<a target="_blank" href="https://www.openstreetmap.org/">© OpenStreetMap 貢獻者</a>',
+      maxZoom: 18,
+    }).addTo(osmMap);
+    this.$http.get('https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json')
+      .then((res) => {
+        this.data = res.data.features;
+        this.updateMarker();
+      });
+  },
+};
+</script>
 
 <style lang="scss">
-@import "bootstrap/scss/bootstrap.scss";
+@import 'bootstrap/scss/bootstrap';
 #map {
   height: 100vh;
+}
+.home {
+  position: relative;
 }
 .highlight {
   background: #e9ffe3;
@@ -71,78 +192,3 @@
   }
 }
 </style>
-
-<script>
-import L from 'leaflet';
-import Taiwan from './assets/Taiwan.json';
-
-let osmMap = {};
-
-console.log(L);
-
-export default {
-  name: 'App',
-  data: () => ({
-    medicineData: [],
-    Taiwan,
-    checkCity: '',
-  }),
-  components: {
-
-  },
-  methods: {
-    updateMap() {
-      const pharmacies = this.medicineData.filter((pharmacy) => (
-        pharmacy.properties.county === this.checkCity));
-
-      pharmacies.forEach((pharmacy) => {
-        L.marker([
-          pharmacy.geometry.coordinates[1],
-          pharmacy.geometry.coordinates[0],
-        ]).addTo(osmMap).bindPopup(`
-        <strong>${pharmacy.properties.name}</strong><br>
-         口罩剩餘：<strong>成人 - ${pharmacy.properties.mask_adult ? `${pharmacy.properties.mask_adult} 個` : '未取得資料'}/ 兒童 - ${pharmacy.properties.mask_child ? `${pharmacy.properties.mask_child} 個` : '未取得資料'}</strong><br>
-        地址: <a href="https://www.google.com.tw/maps/place/${pharmacy.properties.address}" target="_blank">${pharmacy.properties.address}</a><br>
-        電話: ${pharmacy.properties.phone}<br>
-        <small>最後更新時間: ${pharmacy.properties.updated}</small>
-        `);
-      });
-      this.panTo(pharmacies[1]);
-    },
-    removeMapMarker() {
-      osmMap.eachLayer((layer) => {
-        if (layer instanceof L.Marker) {
-          osmMap.removeLayer(layer);
-        }
-      });
-    },
-    penTo(item) {
-      // const icon = item.mask_adult || item.mask_child ? icons.green : icons.grey;
-      osmMap.panTo([item.geometry.coordinates[1], item.geometry.coordinates[0]]);
-    },
-  },
-  mounted() {
-    const url = 'https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json';
-    this.axios.get(url).then((response) => {
-      console.log(response);
-      this.medicineData = response.data.features;
-      this.updateMap();
-    });
-
-
-    osmMap = L.map('map', {
-      center: [25.03, 121.55],
-      zoom: 15,
-    });
-
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-    }).addTo(osmMap);
-
-    // L.marker().addTo(osmMap);
-  },
-
-};
-</script>
